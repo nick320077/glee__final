9cae2b68237a0a9d9060ead82efcf9245cb24068
Nick <kobets32@gmail.com>
2022-08-31 22:17:19 +0300

    added product-details

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,49 @@
 $(function () {
+  $('.related__slider').slick({
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 4,
+    draggable: true,
+  });
+
+  $('.products-tabs__btn').on('click', function (e) {
+    e.preventDefault();
+  $('.products-tabs__btn').removeClass('products-tabs__btn--active');
+  $(this).addClass('products-tabs__btn--active');
+
+  $('.products-tabs__tab').removeClass('products-tabs__tab--active');
+  $($(this).attr('href')).addClass('products-tabs__tab--active');
+
+  });
+
+ 
+
+  $('.product-slide__thumb').slick({
+     asNavFor: '.product-slide__big',
+     focusOnSelect: true,
+     slidesToShow: 3,
+     slidesToScroll: 1,
+     vertical: true,
+     draggable: false,
+  });
+  $('.product-slide__big').slick({
+     asNavFor: '.product-slide__thumb',
+     draggable: false,
+     arrows: false,
+  });
+  $('.product-item__input').styler();
+
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
     autoplay: true,
     autoplaySpeed: 2000,
+  });
+  $(".star").rateYo({
+    starWidth: "18px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
   });
   var mixer = mixitup('.products-items');
   var mixer = mixitup('.design__items');
--- gulpfile.js
+++ gulpfile.js
@@ -35,6 +35,8 @@ function scripts() {
         'node_modules/mixitup/dist/mixitup.js',
         'node_modules/slick-carousel/slick/slick.js',
         'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+        'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
+        'node_modules/rateyo/src/jquery.rateyo.js',
         'app/js/main.js'
     ])
         .pipe(concat('main.min.js'))
